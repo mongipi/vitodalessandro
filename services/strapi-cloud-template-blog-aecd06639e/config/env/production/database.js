9bfefad6a2b37da94fe3c35b7144cb4d19a61d79
pietr <pietromongiello@gmail.com>
2025-09-21 12:56:05 +0200

    add strapi

--- services/strapi-cloud-template-blog-aecd06639e/config/env/production/database.js
+++ services/strapi-cloud-template-blog-aecd06639e/config/env/production/database.js
@@ -2,7 +2,7 @@ module.exports =  ({ env }) => ({
 	connection: {
 		client: 'postgres',
 		connection: {
-		host: env('DATABASE_HOST', '214.154.4.79'),
+		host: env('DATABASE_HOST', '217.154.4.79'),
 			port: env.int('DATABASE_PORT', 5432),
 			database: env('DATABASE_NAME', 'strapi'),
 			user: env('DATABASE_USERNAME', 'strapi'),
